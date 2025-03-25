10d48767fcacdc6e0341aef81619f4f79e92ed06
Karthik <karthikchembhc@gmail.com>
2025-03-25 12:46:42 +0530

    auth fix 3

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,21 +1,3 @@
-import NextAuth from "next-auth"
-import Google from "next-auth/providers/google"
- 
-const handler = NextAuth({
-  providers: [
-    Google({
-      authorization: {
-        params: {
-          prompt: "consent",
-          access_type: "offline",
-          response_type: "code",
-        },
-      },
-      clientId: process.env.AUTH_GOOGLE_ID,
-      clientSecret: process.env.AUTH_GOOGLE_SECRET,
-    }),
-  ],
-})
 
-// Export the Next.js API route handlers
-export { handler as GET, handler as POST }
+import { handlers } from "@/auth"
+export const { GET, POST } = handlers
